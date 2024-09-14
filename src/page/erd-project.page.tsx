f5f8604dd5ea77634e6eb9689099c6550d193d31
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2024-09-14 21:44:55 +0900

    feat: add `erd-project.page`

---
+++ src/page/erd-project.page.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export function ErdProjectPage() {
+  return <styles.container></styles.container>;
+}
+
+const styles = {
+  container: styled.div`
+    width: 100%;
+    height: 100%;
+  `,
+};
