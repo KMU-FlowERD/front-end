b47970eeed1004662079b8c14a6b21f2d70a3846
cjeongmin <cjeongmin27@gmail.com>
2024-10-05 03:20:40 +0900

    feat: Add ERD data state and actions

--- src/__tests__/erd-project.slice.test.ts
+++ src/__tests__/erd-project.slice.test.ts
@@ -1,135 +1,459 @@
 import { describe, expect, test } from '@jest/globals';
-
 import {
   createERDProjectStore,
+  ERDProject,
   ERDRelation,
-  type ERDTable,
+  ERDTable,
 } from '@/features/erd-project';
 
-describe('사용자의 프로젝트 관련 데이터 테스트', () => {
+describe('ERD Project Store', () => {
   const table1: ERDTable = {
     id: 'table1',
+    title: 'Table 1',
     top: 0,
     left: 0,
     width: 100,
     height: 100,
+    columns: [],
   };
+
   const table2: ERDTable = {
     id: 'table2',
+    title: 'Table 2',
     top: 0,
     left: 0,
     width: 100,
     height: 100,
+    columns: [],
   };
+
   const relation: ERDRelation = {
     id: 'relation1',
+    from: 'table1',
+    to: 'table2',
     type: 'one-to-one',
-    from: table1.id,
-    to: table2.id,
-    identify: false,
+    identify: true,
   };
 
-  describe('Project 관련', () => {
-    test('Project 설명이 정상적으로 변경되어야 합니다.', () => {
+  describe('프로젝트 관련 액션', () => {
+    test('프로젝트를 설정합니다.', () => {
       const store = createERDProjectStore();
+      const newProject: ERDProject = {
+        id: 'newProject',
+        title: 'New Project',
+        width: 500,
+        height: 500,
+        tables: [],
+        relations: {},
+      };
 
-      store
-        .getState()
-        .updateDescriptions({ title: 'title', description: 'description' });
-
-      expect(store.getState().title).toBe('title');
-      expect(store.getState().description).toBe('description');
+      store.getState().setProject(newProject);
+      expect(store.getState().id).toBe('newProject');
+      expect(store.getState().title).toBe('New Project');
     });
 
-    test('Project의 캔버스 크기가 변경되어야 합니다.', () => {
+    test('프로젝트 설명을 업데이트합니다.', () => {
       const store = createERDProjectStore();
+      store.getState().updateDescriptions({
+        title: 'Updated Title',
+        description: 'Updated Description',
+      });
 
-      store.getState().updateCanvasSize({ width: 100, height: 100 });
-
-      expect(store.getState().width).toBe(100);
-      expect(store.getState().height).toBe(100);
+      expect(store.getState().title).toBe('Updated Title');
+      expect(store.getState().description).toBe('Updated Description');
     });
 
-    test('Project의 캔버스 크기가 0 또는 음수인 경우 오류를 반환합니다.', () => {
+    test('캔버스 크기를 업데이트합니다.', () => {
       const store = createERDProjectStore();
+      store.getState().updateCanvasSize({ width: 800, height: 600 });
+
+      expect(store.getState().width).toBe(800);
+      expect(store.getState().height).toBe(600);
 
       expect(() =>
-        store.getState().updateCanvasSize({ width: -1, height: -1 }),
-      ).toThrowError(new Error('width, height must greater than 0'));
+        store.getState().updateCanvasSize({ width: -100, height: 600 }),
+      ).toThrow();
     });
   });
 
-  describe('ERD Table 관련', () => {
-    test('Table 추가가 정상적으로 이루어져야 합니다.', () => {
+  describe('테이블 관련 액션', () => {
+    test('테이블을 생성합니다.', () => {
       const store = createERDProjectStore();
-
       store.getState().createTable(table1);
 
       expect(store.getState().tables.length).toBe(1);
+      expect(store.getState().tables[0].title).toBe('Table 1');
     });
 
-    test('Table 업데이트가 정상적으로 이루어져야 합니다.', () => {
+    test('테이블을 업데이트합니다.', () => {
       const store = createERDProjectStore();
       store.getState().createTable(table1);
 
-      store.getState().updateTable({ ...table1, top: 100, height: 100 });
+      const updatedTable = { ...table1, title: 'Updated Table 1' };
+      store.getState().updateTable(updatedTable);
 
-      const table = store.getState().tables.find((t) => t.id === table1.id);
-      expect(table).not.toBeUndefined();
-      expect(table?.top).toBe(100);
-      expect(table?.height).toBe(100);
+      expect(store.getState().tables[0].title).toBe('Updated Table 1');
     });
 
-    test('Table 삭제가 정상적으로 이루어져야 합니다.', () => {
+    test('테이블을 삭제합니다.', () => {
       const store = createERDProjectStore();
       store.getState().createTable(table1);
-
       store.getState().deleteTable(table1.id);
 
       expect(store.getState().tables.length).toBe(0);
     });
   });
 
-  describe('ERD Relation 관련', () => {
-    test('Relation 생성이 정상적으로 이루어져야 합니다.', () => {
+  describe('컬럼 관련 액션', () => {
+    test('컬럼을 생성합니다.', () => {
       const store = createERDProjectStore();
-
       store.getState().createTable(table1);
-      store.getState().createTable(table2);
-      store.getState().createRelation(relation);
+      store.getState().createColumn(table1, true);
 
-      expect(store.getState().relations[table1.id]).not.toBeUndefined();
-      expect(store.getState().relations[table2.id]).not.toBeUndefined();
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 1');
+
+      expect(table).not.toBeUndefined();
+      expect(table?.columns.length).toBe(1);
     });
 
-    test('Relation 수정이 정상적으로 이루어져야 합니다.', () => {
+    test('PK인 컬럼이 추가되면, 릴레이션에 따라 영향을 미칩니다.', () => {
       const store = createERDProjectStore();
-
       store.getState().createTable(table1);
       store.getState().createTable(table2);
       store.getState().createRelation(relation);
+
+      expect(
+        store.getState().tables.find((t) => t.id === table1.id)?.columns.length,
+      ).toBe(0);
+      expect(
+        store.getState().tables.find((t) => t.id === table2.id)?.columns.length,
+      ).toBe(0);
+
+      store.getState().createColumn(table1, true);
+
+      expect(
+        store.getState().tables.find((t) => t.id === table1.id)?.columns.length,
+      ).toBe(1);
+      expect(
+        store.getState().tables.find((t) => t.id === table2.id)?.columns.length,
+      ).toBe(1);
+    });
+
+    test('PK가 아닌 컬럼이 추가되면, 릴레이션과 상관 없이 영향을 미쳐선 안 됩니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createRelation(relation);
+
+      expect(
+        store.getState().tables.find((t) => t.id === table1.id)?.columns.length,
+      ).toBe(0);
+      expect(
+        store.getState().tables.find((t) => t.id === table2.id)?.columns.length,
+      ).toBe(0);
+
+      store.getState().createColumn(table1, false);
+
+      expect(
+        store.getState().tables.find((t) => t.id === table1.id)?.columns.length,
+      ).toBe(1);
+      expect(
+        store.getState().tables.find((t) => t.id === table2.id)?.columns.length,
+      ).toBe(0);
+    });
+
+    test('컬럼을 업데이트합니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createColumn(table1, true);
+
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 1');
+      const column = table?.columns[0];
+      if (column) {
+        const updatedColumn = { ...column, name: 'Updated Column' };
+        store.getState().updateColumn(table1, updatedColumn);
+
+        const updatedTable = store
+          .getState()
+          .tables.find((table) => table.title === 'Table 1');
+        const updatedCol = updatedTable?.columns.find(
+          (col) => col.id === column.id,
+        );
+        expect(updatedCol?.name).toBe('Updated Column');
+      }
+    });
+
+    test('테이블1의 컬럼이 업데이트되면 테이블2에 영향을 미칩니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createColumn(table1, true);
+      store.getState().createRelation(relation);
+
+      const table = store.getState().tables.find((t) => t.id === 'table1');
+      expect(table).not.toBeUndefined();
+
+      const column = table?.columns[0];
+      expect(column).not.toBeUndefined();
+
+      store.getState().updateColumn(table!, { ...column!, name: 'test-name' });
+
+      const updatedTable1 = store
+        .getState()
+        .tables.find((t) => t.id === 'table1');
+      const updatedTable2 = store
+        .getState()
+        .tables.find((t) => t.id === 'table2');
+
+      const updatedColumn1 = updatedTable1?.columns.find(
+        (c) => c.name === 'test-name',
+      );
+      const updatedColumn2 = updatedTable2?.columns.find(
+        (c) => c.name === 'test-name',
+      );
+
+      expect(updatedColumn1).not.toBeUndefined();
+      expect(updatedColumn2).not.toBeUndefined();
+      expect(updatedColumn1!.name).toBe('test-name');
+      expect(updatedColumn2!.name).toBe('test-name');
+    });
+
+    test('컬럼을 삭제합니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createColumn(table1, true);
+
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 1');
+      const column = table?.columns[0];
+      if (column) {
+        store.getState().deleteColumn(table1, column);
+
+        const updatedTable = store
+          .getState()
+          .tables.find((table) => table.title === 'Table 1');
+        expect(updatedTable?.columns.length).toBe(0);
+      }
+    });
+
+    test('컬럼을 삭제하면, PK인 경우 릴레이션에 따라 영향을 미칩니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createRelation(relation);
+      store.getState().createColumn(table1, true);
+
+      const table1Before = store
+        .getState()
+        .tables.find((t) => t.id === 'table1');
+      const table2Before = store
+        .getState()
+        .tables.find((t) => t.id === 'table2');
+      expect(table1Before?.columns.length).toBe(1);
+      expect(table2Before?.columns.length).toBe(1);
+
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 1');
+      const column = table?.columns[0];
+      if (column) {
+        store.getState().deleteColumn(table1, column);
+
+        const updatedTable1 = store
+          .getState()
+          .tables.find((t) => t.id === 'table1');
+        const updatedTable2 = store
+          .getState()
+          .tables.find((t) => t.id === 'table2');
+
+        expect(updatedTable1?.columns.length).toBe(0);
+        expect(updatedTable2?.columns.length).toBe(0);
+      }
+    });
+
+    test('컬럼을 삭제하면, PK가 아닌 경우 릴레이션과 상관 없이 영향을 미쳐선 안 됩니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createRelation(relation);
+      store.getState().createColumn(table1, false);
+
+      const table1Before = store
+        .getState()
+        .tables.find((t) => t.id === 'table1');
+      const table2Before = store
+        .getState()
+        .tables.find((t) => t.id === 'table2');
+      expect(table1Before?.columns.length).toBe(1);
+      expect(table2Before?.columns.length).toBe(0);
+
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 1');
+      const column = table?.columns[0];
+      if (column) {
+        store.getState().deleteColumn(table1, column);
+
+        const updatedTable1 = store
+          .getState()
+          .tables.find((t) => t.id === 'table1');
+        const updatedTable2 = store
+          .getState()
+          .tables.find((t) => t.id === 'table2');
+
+        expect(updatedTable1?.columns.length).toBe(0);
+        expect(updatedTable2?.columns.length).toBe(0);
+      }
+    });
+  });
+
+  describe('릴레이션 관련 액션', () => {
+    test('릴레이션을 생성합니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createRelation(relation);
+
+      expect(store.getState().relations[table1.id].length).toBe(1);
+      expect(store.getState().relations[table2.id].length).toBe(1);
+    });
+
+    test('릴레이션이 생성되면 다른 테이블의 컬럼에 영향을 미칩니다. 이 경우엔 식별 관계입니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createColumn(table1, true);
+      store.getState().createColumn(table2, true);
+      store.getState().createRelation(relation);
+
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 2');
+
+      expect(table).not.toBeUndefined();
+      expect(table?.columns.length).toBe(2);
+    });
+
+    test('릴레이션이 생성되면 다른 테이블의 컬럼에 영향을 미칩니다. 이 경우엔 비식별 관계입니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createColumn(table1, true);
+      store.getState().createColumn(table2, true);
+      store.getState().createRelation({
+        ...relation,
+        identify: false,
+      });
+
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 2');
+
+      expect(table).not.toBeUndefined();
+      expect(table?.columns.length).toBe(2);
+    });
+
+    test('릴레이션을 업데이트합니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createRelation(relation);
+
+      const updatedRelation: ERDRelation = { ...relation, type: 'one-to-many' };
+      store.getState().updateRelation(updatedRelation);
+
+      const updatedRel = store
+        .getState()
+        .relations[table1.id].find((rel) => rel.id === relation.id);
+      expect(updatedRel?.type).toBe('one-to-many');
+    });
+
+    test('릴레이션의 식별/비식별이 변경되면, 다른 테이블에 영향을 미치게됩니다. 식별 → 비식별', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createColumn(table1, true);
+      store.getState().createRelation(relation);
+
+      const beforeTable2 = store
+        .getState()
+        .tables.find((table) => table.id === relation.to);
+
+      expect(beforeTable2?.columns.length).toBe(1);
+      expect(beforeTable2?.columns[0].keyType).toBe('pk/fk');
+
+      store.getState().updateRelation({ ...relation, identify: false });
+
+      const afterTable2 = store
+        .getState()
+        .tables.find((table) => table.id === relation.to);
+
+      expect(afterTable2?.columns.length).toBe(1);
+      expect(afterTable2?.columns[0].keyType).toBe('fk');
+    });
+
+    test('릴레이션의 식별/비식별이 변경되면, 다른 테이블에 영향을 미치게됩니다. 비식별 → 식별', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createColumn(table1, true);
+      store.getState().createRelation({ ...relation, identify: false });
+
+      const beforeTable2 = store
+        .getState()
+        .tables.find((table) => table.id === relation.to);
+
+      expect(beforeTable2?.columns.length).toBe(1);
+      expect(beforeTable2?.columns[0].keyType).toBe('fk');
+
       store.getState().updateRelation({ ...relation, identify: true });
 
-      const target = store
+      const afterTable2 = store
         .getState()
-        .relations[table1.id].find((v) => v.id === relation.id);
+        .tables.find((table) => table.id === relation.to);
 
-      expect(target).not.toBeUndefined();
-      expect(target?.identify).toBe(true);
+      expect(afterTable2?.columns.length).toBe(1);
+      expect(afterTable2?.columns[0].keyType).toBe('pk/fk');
     });
 
-    test('Relation 삭제가 정상적으로 이루어져야 합니다.', () => {
+    test('릴레이션을 삭제합니다.', () => {
       const store = createERDProjectStore();
-
       store.getState().createTable(table1);
       store.getState().createTable(table2);
       store.getState().createRelation(relation);
+
       store.getState().deleteRelation(relation);
 
-      expect(store.getState().relations[table1.id]).not.toBeUndefined();
-      expect(store.getState().relations[table2.id]).not.toBeUndefined();
       expect(store.getState().relations[table1.id].length).toBe(0);
       expect(store.getState().relations[table2.id].length).toBe(0);
+    });
+
+    test('릴레이션을 삭제하면, 다른 테이블의 컬럼에 영향을 미칩니다.', () => {
+      const store = createERDProjectStore();
+      store.getState().createTable(table1);
+      store.getState().createTable(table2);
+      store.getState().createColumn(table1, true);
+      store.getState().createColumn(table2, true);
+      store.getState().createRelation(relation);
+
+      const table = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 2');
+
+      expect(table).not.toBeUndefined();
+      expect(table?.columns.length).toBe(2);
+
+      store.getState().deleteRelation(relation);
+
+      const updatedTable = store
+        .getState()
+        .tables.find((table) => table.title === 'Table 2');
+      expect(updatedTable?.columns.length).toBe(1);
     });
   });
 });
