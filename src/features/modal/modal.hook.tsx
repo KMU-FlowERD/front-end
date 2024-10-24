41bca9a14a9741a13720f64698a0da84d6acba06
cjeongmin <cjeongmin27@gmail.com>
2024-10-25 00:12:00 +0900

    [FLOW-23] feat: add modal feature

---
+++ src/features/modal/modal.hook.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import { ReactNode, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+const Background = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100dvw;
+  height: 100dvh;
+  justify-content: center;
+  align-items: center;
+  background: rgba(47, 47, 47, 0.8);
+  backdrop-filter: blur(4px);
+`;
+
+export const useModal = (children: ReactNode) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+
+  const closeModal = () => setIsOpen(false);
+
+  const Modal = useMemo(() => {
+    if (!isOpen) return null;
+    return createPortal(
+      <Background onClick={closeModal}>{children}</Background>,
+      document.body,
+    );
+  }, [isOpen, children]);
+
+  return {
+    Modal,
+    openModal,
+    closeModal,
+  };
+};
