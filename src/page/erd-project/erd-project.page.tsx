22da2cb39bcadebe2856f3b7a9e9bcf0530fb639
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2024-10-28 17:50:43 +0900

    [FLOW-24] fix: erd page split style

--- src/page/erd-project/erd-project.page.tsx
+++ src/page/erd-project/erd-project.page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import styled from '@emotion/styled';
 import { useState } from 'react';
+
+import { styles } from './erd-project.page.styles';
 
 import {
   ErdDrawTools,
@@ -179,24 +180,3 @@ export function ErdProjectPage() {
     </styles.displayWrapper>
   );
 }
-
-const styles = {
-  displayWrapper: styled.div<{ $pos: { width: number; height: number } }>`
-    position: relative;
-    width: ${({ $pos }) => `${$pos.width}px`};
-    height: ${({ $pos }) => `${$pos.height}px`};
-    background: #2f2f2f;
-  `,
-  container: styled.div`
-    position: fixed;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    padding: 16px;
-    align-items: flex-end;
-    gap: 16px;
-    background: transparent;
-    pointer-events: none;
-    z-index: 2;
-  `,
-};
