ea5ed2c116d28a81eef419dbd46b7a84e4cf11fe
dbtkdfhr <dbtkdfhr2000@kookmin.ac.kr>
2024-10-31 01:28:32 +0900

    [FLOW-4] feat: add relations dropdown

--- src/components/notation-modal/NotationModal.styles.ts
+++ src/components/notation-modal/NotationModal.styles.ts
@@ -8,6 +8,32 @@ export const styles = {
     background: #222;
     justify-content: center;
     align-items: center;
+    padding: 16px;
+    border: 1px solid #444;
+    gap: 20px;
     z-index: 30;
+  `,
+
+  relationWrapper: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+  `,
+
+  dropdownWrapper: styled.div`
+    width: 100px;
+    height: 30px;
+    background: #222;
+    border: 1px solid #444;
+  `,
+
+  dropdown: styled.li`
+    width: 100px;
+    height: 30px;
+    background: #222;
+    justify-content: center;
+    align-items: center;
   `,
 };
--- src/components/notation-modal/NotationModal.tsx
+++ src/components/notation-modal/NotationModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { styles } from './NotationModal.styles';
 
 import { ERDRelation } from '@/features/erd-project';
@@ -5,10 +7,58 @@ import { ERDRelation } from '@/features/erd-project';
 export function NotationModal({ relations }: { relations: ERDRelation[] }) {
   return (
     <styles.container>
-      if you change notation set default type
+      if you change notation, set default
       {relations.map((relation) => (
-        <input key={relation.id} defaultValue={relation.id} />
+        <styles.relationWrapper key={relation.id}>
+          {`${relation.from.substring(0, 5)}_${relation.to.substring(0, 5)}`}:
+          <TypeDropDown />
+          <ParentMultiplicityDropDown />
+        </styles.relationWrapper>
       ))}
     </styles.container>
   );
+}
+
+function TypeDropDown() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <styles.dropdownWrapper onClick={() => setOpen(!open)}>
+      <TypeMenu open={open} />
+    </styles.dropdownWrapper>
+  );
+}
+
+function TypeMenu({ open }: { open: boolean }) {
+  if (open)
+    return (
+      <>
+        <styles.dropdown>1:1</styles.dropdown>
+        <styles.dropdown>1:M</styles.dropdown>
+        <styles.dropdown>N:M</styles.dropdown>
+      </>
+    );
+
+  return null;
+}
+
+function ParentMultiplicityDropDown() {
+  const [open, setOpen] = useState(false);
+  return (
+    <styles.dropdownWrapper onClick={() => setOpen(!open)}>
+      <ParentMultiplicityMenu open={open} />
+    </styles.dropdownWrapper>
+  );
+}
+
+function ParentMultiplicityMenu({ open }: { open: boolean }) {
+  if (open)
+    return (
+      <>
+        <styles.dropdown>전체참여</styles.dropdown>
+        <styles.dropdown>부분참여</styles.dropdown>
+      </>
+    );
+
+  return null;
 }
